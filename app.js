6f6b28411103b7de5c007f7a36b4eb77aa7f39ba
STARTERGATE <32726809+startergate@users.noreply.github.com>
2018-10-11 20:07:48 +0900

    view 기능 부분 기본

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 /*jshint esversion: 6*/
 
+
 var http = require('http');
 var url = require('url');
 var qs = require('querystring');
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://디비주소";
 var mongoose = require('mongoose');
 
 var app = express();
@@ -44,19 +47,30 @@ app.get('/boardWork/list', function (req, res) {
 });
 
 app.get('/boardWork/view', function (req, res) {
+  var board = req.query('board');
   var postId = req.query('postid');
+  var sendData;
 
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    dbo.collection("tb_board").find({"board": board}, function(err, result) {
+      if (err) throw err;
+      sendData = result;
+      db.close();
+    });
+  });
   // DB 데이터 받고
-  var sendData =
-  {
-      'board': boardName,
-      'name': '글 제목',
-      'postTime': '게시 시간',
-      'visible': true,
-      'writerID': '게시 ID',
-      'hits': 0,
-      'content': ``
-  };
+  //var sendData =
+  //{
+  //    'board': boardName,
+  //    'name': '글 제목',
+  //    'postTime': '게시 시간',
+  //    'visible': true,
+  //    'writerID': '게시 ID',
+  //    'hits': 0,
+  //    'content': ``
+  //};
   res.send(sendData);
 });
 app.get('/boardWork/getPostName', function (req, res) {
